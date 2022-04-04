1e88faefb164af22c7f68495f4bd5f8cd6b52e0f
Germán Freixinós López <german.freixinos-lopez@capgemini.com>
2022-04-04 17:25:30 +0200

    Add IProductoService class

---
+++ src/main/java/com/capgemini/service/IProductoService.java
@@ -0,0 +1,21 @@
+package com.capgemini.service;
+
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
+
+import com.capgemini.entities.Producto;
+
+public interface IProductoService {
+	public List<Producto> findAll(Sort sort);
+
+	public Page<Producto> findAll(Pageable pageable);
+
+	public Producto findById(long id);
+
+	public void delete(long id);
+
+	public Producto save(Producto producto);
+}
